d67277d3765c54d91a6ef36c15d1a1458cf3d5b9
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-24 17:35:28 +0800

    <multiplication table> <Carol> <generate a multiplication table in range>

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,7 +1,83 @@
 package com.tw;
+/*PDCA
+P:
+D:
+C:
+A:
+*/
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 public class MultiplicationTable {
-    public String create(int startNumber, int endNumber) {
-        return null;
+    public String generateMultiplicationTableWithRange(int startNumber, int endNumber) {
+        boolean isValid;
+        isValid = isValidInputNums(startNumber, endNumber);
+        if(isValid) {
+            String tableResult = "";
+                tableResult = generateMultipleTable(startNumber, endNumber);
+            return tableResult;
+        }
+        else{
+            return null;
+        }
     }
+    public boolean isValidInputNums(int startNumber, int endNumber) {
+        Boolean isstartNumberInRange;
+        Boolean isendNumberInRange;
+
+        Boolean compareResult;
+        isstartNumberInRange = isInputNumsInRange(startNumber);
+        isendNumberInRange = isInputNumsInRange(startNumber);
+        compareResult = isEndNumLargerThanStartNum(startNumber, endNumber);
+        if(isstartNumberInRange && isendNumberInRange && compareResult){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean isInputNumsInRange(int input){
+        if (input >= 1 && input <= 1000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean isEndNumLargerThanStartNum(int startNumber, int endNumber){
+        if(endNumber>=startNumber) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public String generateMultipleTable(int startNumber, int endNumber){
+        String tableResult="";
+        for(int i = startNumber; i<=endNumber; i++){
+            if(i!=endNumber) {
+                tableResult += generateMultipleLine(startNumber, i) + "\r\n";
+            }
+            else
+                tableResult += generateMultipleLine(startNumber, i);
+        }
+        return tableResult;
+    }
+    public String generateMultipleLine(int startNum, int endNum){
+        String multipleline="";
+        for(int i= startNum ; i<=endNum; i++){
+            if(i!=endNum) {
+                multipleline += generateMultiplicationExpression(i, endNum) + "  ";
+            }
+            else
+                multipleline += generateMultiplicationExpression(i, endNum);
+        }
+       return multipleline;
+    }
+    public String generateMultiplicationExpression(int operand, int iterate){
+        String multiplicationExpression ="";
+        multiplicationExpression = operand + "*" + iterate +"=" + operand*iterate;
+        return multiplicationExpression;
+    }
+
+
 }
